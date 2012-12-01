18d4d0a6735fcf5a7c34df646abad2baa39127b5
Martha Njeri <martha@martha-VirtualBox.(none)>
2012-12-01 21:51:08 +0000

    Updates to web site for scrolling and the link to the forums

--- support.php
+++ support.php
@@ -6,7 +6,7 @@
       <p>As an open-source application, the choices for support are extensive. Free support is available provided there are volunteers to offer it. Developers and users frequent the mailing list and forum and if you have specific questions illustrating your issue or what you wish to acheive asking your question on either the mailing list or forum will provide the answers you need. However, if you are using <i>KwaMoja</i> and need the backing and guaranttee of a response within a reasonable time-frame then commercial support is also available from a number of providers. <i>KwaMoja</i> cannot endorse any particular commercial support provider, although many of the developers and contributors are actively engaged in businesses that support <i>KwaMoja</i>. </p>
 		<h1>Forums</h1>
 		<img src="images/forums.png" style="float: right" />
-		<p>A good place to start looking for support is on our <a href="" target="_blank">forums</a>. These cover a wide range of topics but if you feel that there is a forum topic that we have not got, then please let us know and we will do our best to oblige.</p>
+		<p>A good place to start looking for support is on our <a href="http://www.kwamoja.com/forums" target="_blank">forums</a>. These cover a wide range of topics but if you feel that there is a forum topic that we have not got, then please let us know and we will do our best to oblige.</p>
 		<p>As with everything related to this project we ask everybody to read and abide by our <a href="" onclick="return OpenSubWindow('CodeOfConduct.php', '<i>KwaMoja</i> Code Of Conduct')">Code of Conduct</a> before signing up for our forums. Abuse of this code will not be tolerated.</p>
 		<img src="images/email.png" style="float: left" />
 		<h1>Mailing lists</h1>
